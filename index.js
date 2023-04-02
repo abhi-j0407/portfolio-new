d3338620722b6a96a77213d8fac3f9c0a7fc0b20
Abhishek Jain <abhishek.j0407@gmail.com>
2023-04-02 19:12:30 +0530

    Basic setup

---
+++ index.js
@@ -0,0 +1,5 @@
+//Function to get rid of preloader
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preloader');
+    preloader.classList.add('preloaded');
+});
